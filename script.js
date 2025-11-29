e56efa6a6c4e4dbbd2e95a79fb9b340f0ab4ffaf
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2025-11-29 15:00:01 +0100

    done first task

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("registration-form");
+    const feedbackDiv = document.getElementById("form-feedback");
+    const usernameinput = document.getElementById("username");
+    const emailinput = document.getElementById("email");
+    const passwordinput = document.getElementById("password");
+
+    form.addEventListener('submit', function  (e) {
+        e.preventDefault();
+        
+        const username = usernameinput.value.trim();
+        const  email = emailinput.value.trim();
+        const password = passwordinput.value.trim();
+
+        let isvalid = true;
+        let messages = [];
+
+        if (username.length < 3) {
+            let isvalid = false;
+            messages.push("User name's too short");
+        }
+
+
+        if (!email.includes ('@') || !email.includes ('.')) {
+            let isvalid = false;
+            messages.push("Invalid email address")
+        }
+  
+        
+        if (password.length < 8) {
+            let isvalid = false;
+            messages.push('You need a stronger password of at least 8 characters')
+        }
+
+        feedbackDiv.style.display = 'block';
+
+        if (!isvalid){
+            feedbackDiv.textContent = messages.join("<br>");
+            feedbackDiv.style.color = '"#dc3545"' ;
+        }
+        else {
+            feedbackDiv.textContent = "Registration successful!";
+            feedbackDiv.style.color = '#28a745';
+        }
+
+
+
+    });
+});
